71c15e7a11d5dc534979fed4ddf8e5729446ccc4
li305263 <164436886@qq.com>
2020-08-19 22:21:58 +0800

    新增 防抖功能

---
+++ src/function/debounce.ts
@@ -0,0 +1,38 @@
+/**
+ * @description: 防抖 如果持续触发事件，停止触发一段时间后，执行一次事件
+ * @param {function} 回调
+ * @param {number} 毫秒数
+ * @param {boolean} 是否立即执行
+ * @return {any}
+ */
+function debounce(fn: Function, wait: number, immediate: boolean): any {
+  let timeout: any, result: any;
+  const _debounce = function (this: void) {
+    if (timeout) clearTimeout(timeout);
+    const ctx = this,
+      args = arguments;
+
+    if (immediate) {
+      // 如果已经执行过，不再执行
+      let callNow = !timeout;
+
+      timeout = setTimeout(() => {
+        timeout = null;
+      }, wait);
+
+      if (callNow) result = fn.apply(ctx, args);
+    } else {
+      timeout = setTimeout(function () {
+        fn.apply(ctx, args);
+      }, wait);
+    }
+    return result;
+  };
+
+  _debounce.cancel = function () {
+    clearTimeout(timeout);
+    timeout = null;
+  };
+
+  return _debounce;
+}
